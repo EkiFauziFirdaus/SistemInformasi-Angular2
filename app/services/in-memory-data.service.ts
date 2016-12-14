4680341f688f8d254e778acfa727a1d7af86569a
exagradia <exagradia@outlook.com>
2016-12-14 08:50:47 +0700

    Menampilkan gambar produk

--- app/services/in-memory-data.service.ts
+++ app/services/in-memory-data.service.ts
@@ -2,14 +2,14 @@ import { InMemoryDbService } from 'angular-in-memory-web-api';
 export class InMemoryDataService implements InMemoryDbService {
   createDb() {
     let produk = [
-      { kodeProduk: 'HP001', kodeMerk: 'HPSAMSUNG', namaProduk: 'Samsung Galaxy S', material: 'Plastik', deskripsiProduk: 'Handphone Samsung Galaxy S', negaraPemroduksi: 'Korea', gambar: './GambarProduk/samsung/GalaxyS.jpg', stok: 100},
-      { kodeProduk: 'HP002', kodeMerk: 'HPSAMSUNG', namaProduk: 'Samsung Galaxy S2', material: 'Plastik', deskripsiProduk: 'Handphone Samsung Galaxy S2', negaraPemroduksi: 'Korea', gambar: './GambarProduk/samsung/GalaxyS2.jpg', stok: 100},
-      { kodeProduk: 'HP003', kodeMerk: 'HPSAMSUNG', namaProduk: 'Samsung Galaxy S3', material: 'Plastik', deskripsiProduk: 'Handphone Samsung Galaxy S3', negaraPemroduksi: 'Korea', gambar: './GambarProduk/samsung/GalaxyS3.jpg', stok: 100},
-      { kodeProduk: 'HP004', kodeMerk: 'HPSAMSUNG', namaProduk: 'Samsung Galaxy S4', material: 'Plastik', deskripsiProduk: 'Handphone Samsung Galaxy S4', negaraPemroduksi: 'Korea', gambar: './GambarProduk/samsung/GalaxyS4.jpg', stok: 100},
-      { kodeProduk: 'HP005', kodeMerk: 'HPSAMSUNG', namaProduk: 'Samsung Galaxy S5', material: 'Plastik', deskripsiProduk: 'Handphone Samsung Galaxy S5', negaraPemroduksi: 'Korea', gambar: './GambarProduk/samsung/GalaxyS5.jpg', stok: 100},
-      { kodeProduk: 'HP006', kodeMerk: 'HPSAMSUNG', namaProduk: 'Samsung Galaxy S6', material: 'Besi', deskripsiProduk: 'Handphone Samsung Galaxy S6', negaraPemroduksi: 'Korea', gambar: './GambarProduk/samsung/GalaxyS6.jpg', stok: 100},
-      { kodeProduk: 'HP007', kodeMerk: 'HPSONY', namaProduk: 'Sony Xperia Z', material: 'Besi', deskripsiProduk: 'Handphone Sony Xperia Z', negaraPemroduksi: 'Jepang', gambar: './GambarProduk/sony/XperiaZ.jpg', stok: 100},
-      { kodeProduk: 'HP008', kodeMerk: 'HPSONY', namaProduk: 'Sony Xperia Z1', material: 'Besi', deskripsiProduk: 'Handphone Sony Xperia Z1', negaraPemroduksi: 'Jepang', gambar: './GambarProduk/sony/XperiaZ1.jpg', stok: 100}
+      { kodeProduk: 'HP001', kodeMerk: 'HPSAMSUNG', namaProduk: 'Samsung Galaxy S', material: 'Plastik', deskripsiProduk: 'Handphone Samsung Galaxy S', negaraPemroduksi: 'Korea', gambar: '../app/assets/img/produk/samsung/GalaxyS.jpg', stok: 100},
+      { kodeProduk: 'HP002', kodeMerk: 'HPSAMSUNG', namaProduk: 'Samsung Galaxy S2', material: 'Plastik', deskripsiProduk: 'Handphone Samsung Galaxy S2', negaraPemroduksi: 'Korea', gambar: '../app/assets/img/produk/samsung/GalaxyS2.jpg', stok: 100},
+      { kodeProduk: 'HP003', kodeMerk: 'HPSAMSUNG', namaProduk: 'Samsung Galaxy S3', material: 'Plastik', deskripsiProduk: 'Handphone Samsung Galaxy S3', negaraPemroduksi: 'Korea', gambar: '../app/assets/img/produk/samsung/GalaxyS3.jpg', stok: 100},
+      { kodeProduk: 'HP004', kodeMerk: 'HPSAMSUNG', namaProduk: 'Samsung Galaxy S4', material: 'Plastik', deskripsiProduk: 'Handphone Samsung Galaxy S4', negaraPemroduksi: 'Korea', gambar: '../app/assets/img/produk/samsung/GalaxyS4.jpg', stok: 100},
+      { kodeProduk: 'HP005', kodeMerk: 'HPSAMSUNG', namaProduk: 'Samsung Galaxy S5', material: 'Plastik', deskripsiProduk: 'Handphone Samsung Galaxy S5', negaraPemroduksi: 'Korea', gambar: '../app/assets/img/produk/samsung/GalaxyS5.jpg', stok: 100},
+      { kodeProduk: 'HP006', kodeMerk: 'HPSAMSUNG', namaProduk: 'Samsung Galaxy S6', material: 'Besi', deskripsiProduk: 'Handphone Samsung Galaxy S6', negaraPemroduksi: 'Korea', gambar: '../app/assets/img/produk/samsung/GalaxyS6.jpg', stok: 100},
+      { kodeProduk: 'HP007', kodeMerk: 'HPSONY', namaProduk: 'Sony Xperia Z', material: 'Besi', deskripsiProduk: 'Handphone Sony Xperia Z', negaraPemroduksi: 'Jepang', gambar: '../app/assets/img/produk/sony/XperiaZ.jpg', stok: 100},
+      { kodeProduk: 'HP008', kodeMerk: 'HPSONY', namaProduk: 'Sony Xperia Z1', material: 'Besi', deskripsiProduk: 'Handphone Sony Xperia Z1', negaraPemroduksi: 'Jepang', gambar: '../app/assets/img/produk/sony/XperiaZ1.jpg', stok: 100}
     ];
     return {produk};
   }
