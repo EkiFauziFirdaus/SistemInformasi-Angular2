e41efd903429c858e101944395676f98f4c65464
exagradia <exagradia@outlook.com>
2016-12-15 08:25:44 +0700

    Menambahkan bootstrap versi 3.3.7

--- app/components/detail-produk.component.ts
+++ app/components/detail-produk.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import { Location } from '@angular/common';
 
+
 import 'rxjs/add/operator/switchMap';
 
 import { Produk } from '../models/produk.model';
